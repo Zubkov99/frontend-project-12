a7a019b74092f756a63c2e76265bac80c11107c5
DimaZ <dimazubkov99@gmail.com>
2022-10-27 12:15:45 +0300

    done 11 step

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import React from "react";
+import { Provider, ErrorBoundary } from '@rollbar/react';
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../../pages/HomePage";
 import LoginPage from "../../pages/LoginPage";
@@ -9,11 +10,29 @@ import AppContext from "../../helpers/сontext";
 import { useLocalStorage } from "../../helpers/useLocalStorage";
 import Layout from "../Layout/Layout";
 
+const rollbarConfig = {
+    accessToken: 'ff503d3bcafc4c20b3a14249c5f53c3b',
+    environment: 'production',
+    server: {
+        root: "http://localhost:3000",
+        branch: "main"
+    },
+    code_version: "0.13.7",
+    person: {
+        id: 117,
+        email: "chief@unsc.gov",
+        username: "john-halo"
+    }
+};
+
+
 const App = () => {
     const [key, setKey] = useLocalStorage(null, 'user');
     const [lang, setLang] = useLocalStorage('ru', 'lang');
     return (
-        <AppContext.Provider value={{
+        <Provider config={rollbarConfig}>
+            <ErrorBoundary>
+            <AppContext.Provider value={{
             key,
             setKey,
             lang,
@@ -28,6 +47,8 @@ const App = () => {
                 </Route>
             </Routes>
         </AppContext.Provider>
+            </ErrorBoundary>
+        </Provider>
     );
 }
 
