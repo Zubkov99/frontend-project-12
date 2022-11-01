e0dad7504078968c10dd9f508603fc700b1f084b
DimaZ <dimazubkov99@gmail.com>
2022-11-02 00:52:03 +0300

    try lint

--- frontend/src/components/ChannelsList/ChannelsList.jsx
+++ frontend/src/components/ChannelsList/ChannelsList.jsx
@@ -9,29 +9,29 @@ import AppContext from '../../helpers/context';
 import RemoveChannelModal from '../RemoveChannelModal';
 
 const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
-    <a
-        href=""
-        ref={ref}
-        onClick={(e) => {
-          e.preventDefault();
-          onClick(e);
-        }}
-        style={{
-          color: '#FFFFFF',
-          textDecoration: 'none',
-        }}
-    >
-        {/* //TODO */}
-        {/* Раскомментить класснейм или выпилить */}
-        <span
-            // className='visually-hidden'
-        >Управление каналом</span>
-        {children}
-        &#x25bc;
-    </a>
+  <a
+    href=""
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+    style={{
+      color: '#FFFFFF',
+      textDecoration: 'none',
+    }}
+  >
+    {/* //TODO */}
+    {/* Раскомментить класснейм или выпилить */}
+    <span>
+      Управление каналом
+    </span>
+    {children}
+    &#x25bc;
+  </a>
 ));
 
-const ChannelsList = (props) => {
+function ChannelsList(props) {
   const { t } = useTranslation();
   const { channels } = props;
   const dispatch = useDispatch();
@@ -51,53 +51,58 @@ const ChannelsList = (props) => {
   const handleShowEditModal = () => setShowEditModal(true);
 
   return (
-        <>
-            <ListGroup variant="flush"
-                       className={styles.ChannelsContainer}
+    <>
+      <ListGroup
+        variant="flush"
+        className={styles.ChannelsContainer}
+      >
+        {channels.map(({
+          name, id, removable, author,
+        }) => {
+          const newName = `# ${name}`;
+          return (
+            <ListGroup.Item
+              action
+              variant="dark"
+              active={activeChannel.id === id}
+              key={id}
+              onClick={() => dispatch(setActiveChannel(id))}
+              className={styles.ListGroup}
             >
-                {channels.map(({
-                  name, id, removable, author,
-                }) => {
-                  const newName = `# ${name}`;
-                  return (
-                        <>
-                            <ListGroup.Item action
-                                            variant="dark"
-                                            active={activeChannel.id === id}
-                                            key={id}
-                                            onClick={() => dispatch(setActiveChannel(id)) }
-                                            className={styles.ListGroup}>
-                                {newName}
-                                { author === key.username
-                                    && <Dropdown>
-                                        <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components"/>
-                                        <Dropdown.Menu>
-                                            <Dropdown.Item onClick={() => {
-                                              setCurrentId(id);
-                                              handleShowEditModal();
-                                            }}>
-                                                {t('chatPage.editChannelButton')}
-                                            </Dropdown.Item>
-                                            <Dropdown.Item onClick={() => {
-                                              if (!removable) return;
-                                              setCurrentId(id);
-                                              handleShowRemoveModal();
-                                              // deleteChannelHandler(id, removable)
-                                              // toast(t('notificationBlock.channelRemoved'));
-                                            }}>
-                                                {t('chatPage.removeChannelButton')}
-                                            </Dropdown.Item>
-                                        </Dropdown.Menu>
-                                    </Dropdown>}
-                            </ListGroup.Item>
-                        </>
-                  );
-                })}
-            </ListGroup>
-            <EditModalWindow show={showEditModal} handleClose={handleCloseEditModal} currentId={currentId}/>
-            <RemoveChannelModal show={showRemoveModal} handleClose={handleCloseRemoveModal} currentId={currentId}/>
-        </>
+              {newName}
+              { author === key.username
+                                    && (
+                                    <Dropdown>
+                                      <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components" />
+                                      <Dropdown.Menu>
+                                        <Dropdown.Item onClick={() => {
+                                          setCurrentId(id);
+                                          handleShowEditModal();
+                                        }}
+                                        >
+                                          {t('chatPage.editChannelButton')}
+                                        </Dropdown.Item>
+                                        <Dropdown.Item onClick={() => {
+                                          if (!removable) return;
+                                          setCurrentId(id);
+                                          handleShowRemoveModal();
+                                          // deleteChannelHandler(id, removable)
+                                          // toast(t('notificationBlock.channelRemoved'));
+                                        }}
+                                        >
+                                          {t('chatPage.removeChannelButton')}
+                                        </Dropdown.Item>
+                                      </Dropdown.Menu>
+                                    </Dropdown>
+                                    )}
+            </ListGroup.Item>
+          );
+        })}
+      </ListGroup>
+      <EditModalWindow show={showEditModal} handleClose={handleCloseEditModal} currentId={currentId} />
+      <RemoveChannelModal show={showRemoveModal} handleClose={handleCloseRemoveModal} currentId={currentId} />
+    </>
   );
-};
+}
 
 export default ChannelsList;
--- frontend/src/components/ChatWindow/ChatWindow.jsx
+++ frontend/src/components/ChatWindow/ChatWindow.jsx
@@ -8,7 +8,7 @@ import Messages from '../Messages';
 import Channels from '../Channels';
 import styles from './ChatWindow.module.css';
 
-const ChatWindow = () => {
+function ChatWindow() {
   const activeChannel = useSelector((state) => state.content.channels.find((item) => item.id === state.content.activeChannelId));
 
   // TODO
@@ -17,28 +17,29 @@ const ChatWindow = () => {
   if (activeChannel) activeChannelName = `# ${activeChannel.name}`;
 
   return (
-        <Card className={styles.cardMain}>
-            <Card.Body className={styles.cardBody}>
-                <Container>
-                    <Row>
-                        <Col xs={4} className={styles.ColChannels}>
-                            <Channels />
-                        </Col>
-                        <Col className={styles.ColMessages}>
-                                {activeChannel
-                                    && <div className={`${styles.activeChannel} shadow-sm`}>
-                                        {/* <h5># {activeChannel.name}</h5> */}
-                                        <h5>{activeChannelName}</h5>
+    <Card className={styles.cardMain}>
+      <Card.Body className={styles.cardBody}>
+        <Container>
+          <Row>
+            <Col xs={4} className={styles.ColChannels}>
+              <Channels />
+            </Col>
+            <Col className={styles.ColMessages}>
+              {activeChannel
+                                    && (
+                                    <div className={`${styles.activeChannel} shadow-sm`}>
+                                      {/* <h5># {activeChannel.name}</h5> */}
+                                      <h5>{activeChannelName}</h5>
                                     </div>
-                                }
-                            <Messages />
-                        </Col>
-                    </Row>
-                </Container>
-            </Card.Body>
-            <ToastContainer />
-        </Card>
+                                    )}
+              <Messages />
+            </Col>
+          </Row>
+        </Container>
+      </Card.Body>
+      <ToastContainer />
+    </Card>
   );
-};
+}
 
 export default ChatWindow;
